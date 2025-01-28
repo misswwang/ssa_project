de4a96839035f7d1e1aa3cc475ba7b51e677ce29
misswwang <97772132+misswwang@users.noreply.github.com>
2025-01-28 16:37:27 +1100

    Handle Join Requests

--- chipin/urls.py
+++ chipin/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
    path('group/<int:group_id>/invite/', views.invite_users, name='invite_users'),
    path('group/<int:group_id>/delete/', views.delete_group, name='delete_group'),
    path('accept-invite/<int:group_id>/', views.accept_invite, name='accept_invite'),
+   path('delete-join-request/<int:request_id>/', views.delete_join_request, name='delete_join_request'),
+   path('group/<int:group_id>/request-to-join/', views.request_to_join_group, name='request_to_join_group'),
+   path('group/<int:group_id>/leave/', views.leave_group, name='leave_group'),
 ]
